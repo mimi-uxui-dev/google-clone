c9f0a456cc7b27c02a6870785fc9cde1399d23d5
mimi-uxui-dev <uxui.mimi@gmail.com>
2023-01-01 21:31:50 +0100

    created Header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -10,7 +11,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Mimi</h1>
+      {/* Header */}
+      <Header />
+      {/* Body */}
+
+      {/* Footer */}
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+const { join } = require("path");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
