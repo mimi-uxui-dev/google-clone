bf4d18280a508806e2bd90aadc5b119263dc4aac
mimi-uxui-dev <uxui.mimi@gmail.com>
2023-01-02 09:45:38 +0100

    adding body section

--- pages/auth/signIn.js
+++ pages/auth/signIn.js
@@ -9,10 +9,12 @@ export default function signin({ providers }) {
       <div className="mt-40">
         {Object.values(providers).map((provider) => (
           <div key={provider.name} className="flex flex-col items-center">
-            <img
+            <Image
               className="w-52 object-cover"
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
               alt="google-logo"
+              width="300"
+              height="100"
             />
             <p className="text-sm italic my-10 text-center">
               This website is created for learning purposes
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
 import Header from "../components/Header";
+import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -11,11 +13,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-      {/* Body */}
 
-      {/* Footer */}
+      <form className="flex flex-col items-center justify-center mx-auto mt-48 w-fit">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          alt="google-logo"
+          width="240"
+          height="80"
+        />
+        <div className="flex my-4 flex-row justify-center items-center gap-4 max-auto border border-gray-200 px-5 py-2 rounded-full hover:shadow-sm focus-within:shadow-sm transition-all">
+          <SearchIcon className="h-5" />
+          <input
+            placeholder="Search Google or type a URL"
+            type="text"
+            className="w-96 flex-grow focus:outline-none "
+          />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-row gap-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">{"I'm feeling Lucky"}</button>
+        </div>
+      </form>
     </>
   );
 }
